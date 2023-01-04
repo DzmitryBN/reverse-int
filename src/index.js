b394ea6d99d17cbfc53d2e1e3036e9854757a0e1
DzmitryBN <DzmitryBN@gmail.com>
2023-01-04 21:14:23 +0100

    feat: implemet function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 module.exports = function reverse(n) {
-    if (n = 0) {
+    if (n === 0) {
         return 0;
     }
 
-    const nToString = toString(n);
-    const nLenght = nToString.length;
+    let nToString = String(n);
+    let nLenght = nToString.length;
+    let firstDigit = nToString[0]
+    const secondDigit=nToString[1]
+    const isNegative=firstDigit==='-'
 
-    if (nLenght === 1) {
-        return n;
+    if (isNegative){
+        nToString=nToString.slice(1)
+        firstDigit = nToString[0]
+        nLenght--;
     }
 
-    const firstDigit = nToString[0]
     let nNew ="";
-    for (let i = nLenght-1; i > 0; n--) {
+    for (let i = nLenght-1; i > 0; i--) {
     nNew +=  nToString [i];
     }
     nNew += firstDigit;
