0227382260285172623463bd0a5134ba960cee51
DzmitryBN <DzmitryBN@gmail.com>
2023-01-04 20:43:42 +0100

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n = 0) {
+        return 0;
+    }
+
+    const nToString = toString(n);
+    const nLenght = nToString.length;
+
+    if (nLenght === 1) {
+        return n;
+    }
+
+    const firstDigit = nToString[0]
+    let nNew ="";
+    for (let i = nLenght-1; i > 0; n--) {
+    nNew +=  nToString [i];
+    }
+    nNew += firstDigit;
+    return parseInt(nNew);
 }
